c4471331cad9bf90ada89e0a75d2d359b1c5d3c5
Joaquin Giordano <joaquin_giordano@outlook.com>
2025-08-26 11:40:59 -0300

    Removing Unnecesary Permission

--- scripts/watchlist-button.js
+++ scripts/watchlist-button.js
@@ -4,11 +4,6 @@ function renderButton(navbar) {
 	const li = document.createElement("li");
 	li.classList.add("navigation-tab");
 
-	// const a = document.createElement("a");
-	// a.textContent = "Custom Watchlist";
-	// a.href = "https://www.netflix.com/browse/my-list?custom=true";
-	// li.appendChild(a);
-
 	const btn = document.createElement("button");
 	btn.textContent = "Custom Watchlists";
 	btn.id = "cw-custom-watchlists-btn";
